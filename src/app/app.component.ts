c66b610d4d6e129fae55c636de10b5d1cfed91b6
Krymova-EN <krymova-en@gamma-center.ru>
2018-10-23 17:05:33 +0300

    --all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  public expanded = false;
+  public countries: Array<string> = ['Russia', 'USA'];
+  public form: FormGroup;
+  public resultForm: Array<any>;
+  public dataOption: Array<string>;
+
+  constructor() {
+    this.createForm();
+    this.form.valueChanges.forEach(result => {
+      this.resultForm = [];
+      this.dataOption = [];
+      for (let key in result) {
+        if (result.hasOwnProperty(key)) {
+          this.resultForm.push({ name: key, check: result[key] });
+          if (result[key]) {
+            this.dataOption.push(key)
+          }
+        }
+      }
+    })
+  }
+
+  createForm() {
+    let group: any = {};
+    for (let country of this.countries) {
+      group[country] = country ? new FormControl(false, Validators.required)
+        : new FormControl(false);
+    };
+    this.form = new FormGroup(group);
+  }
+
+  select(country) {
+    if (this.form.get(country).value === true) {
+      this.form.get(country).setValue(false);
+    } else {
+      this.form.get(country).setValue(true);
+    }
+  }
+
+  showCheckboxes() {
+    if (!this.expanded) {
+      this.expanded = true;
+    } else {
+      this.expanded = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
